225269217dbc534ac0d71ff05e2d5bb26b4fc118
Kolmar Kafran <kolmar@kafran.net>
2020-04-06 12:59:53 -0300

    Initial commit

---
+++ asyncpy.py
@@ -0,0 +1,37 @@
+import asyncio
+import janus
+import random
+
+_sentinel = object()
+
+
+def threaded(sync_q):
+    while True:
+        for val in iter(sync_q.get, _sentinel):
+            print(val)
+            sync_q.task_done()
+
+
+async def async_coro1(async_q):
+    for i in range(10):
+        await async_q.put(f"Coro 1: {i}")
+        await asyncio.sleep(1)
+    await async_q.join()
+
+
+async def main():
+    q1 = janus.Queue()
+    q2 = asyncio.Queue()
+
+    for i in range(30):
+        q2.put_no
+
+    # done, pending = await asyncio.wait()
+    # if not pending:
+    #     await q1.async_q.put(_sentinel)
+
+    loop = asyncio.get_running_loop()
+    loop.run_in_executor(None, threaded, q1.sync_q)
+
+
+asyncio.run(main())
