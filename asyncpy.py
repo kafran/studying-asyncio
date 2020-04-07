b17fe1dc5866b995497b36ae9f2bb6be89093179
Kolmar Kafran <kolmar@kafran.net>
2020-04-07 20:11:31 -0300

    Working script

--- asyncpy.py
+++ asyncpy.py
@@ -5,37 +5,75 @@ import random
 _sentinel = object()
 
 
-def threaded(sync_q):
+# I tryied using another function to emulate an I/O job
+# but I was getting a <coroutine object Queue.get at ...>
+# on Thread.
+
+# async def work_on_task(task):
+#     # This simulate an I/O job
+#     iotime = random.randint(1, 5)
+#     await asyncio.sleep(iotime)
+#     return f"Task {task} took {iotime}."
+
+
+def thread_worker(sync_q):
+    # Emulates a job with some sync lib
+    # sync_q is Janus Sync Queue
     while True:
-        for val in iter(sync_q.get, _sentinel):
-            print(val)
-            sync_q.task_done()
+        task = sync_q.get()
+        if task is _sentinel:
+            break
+        # Process the task
+        print(f"Task printed on Thread: ({task}).")
+        sync_q.task_done()
 
 
-async def async_worker(name, async_q):
-    for i in range(10):
-        await async_q.put(f"Coro 1: {i}")
-        await asyncio.sleep(1)
-    await async_q.join()
+async def async_worker(name, consumer_q, producer_q):
+    # Consumer Queue is Asyncio Queue
+    # Producer Queue is Janus Async Queue to comunicate with Thread
+    while True:
+        task = await consumer_q.get()  # should I await on this?
+        iotime = random.randint(1, 5)
+        await asyncio.sleep(iotime)
+        res = f"{name} worked on task {task} in {iotime} seconds."
+        producer_q.put_nowait(res)  # should I await on this?
+        await producer_q.join()  # should I await on this?
+        consumer_q.task_done()  # should I await on this?
 
 
 async def main():
-    q1 = janus.Queue()
-    q2 = asyncio.Queue()
+    janus_q = janus.Queue()
+    async_q = asyncio.Queue()
 
-    for i in range(30):
-        q2.put_nowait(i + 1)
+    # This generates items to be processed.
+    for i in range(10):
+        async_q.put_nowait(i + 1)
 
+    # This will limit the number of concurrent tasks
     tasks = []
-    for i in range(2):
-        task = asyncio.create_task()
-
-    # done, pending = await asyncio.wait()
-    # if not pending:
-    #     await q1.async_q.put(_sentinel)
-
+    for i in range(3):
+        task = asyncio.create_task(
+            async_worker(
+                name=f"Worker-{i}",
+                consumer_q=async_q,
+                producer_q=janus_q.async_q,
+            )
+        )
+    # Start the Thread side to work on Sync Lib.
     loop = asyncio.get_running_loop()
-    loop.run_in_executor(None, threaded, q1.sync_q)
+    loop.run_in_executor(None, thread_worker, janus_q.sync_q)
+
+    # How to stop verything?
+    # Lets try this based on the example on Python Async doc:
+    await async_q.join()
+    # await row_q.async_q.join() # Should I wait on Janus Queue too?
+
+    # done, pending = await asyncio.wait(tasks)
+
+    for task in tasks:
+        task.cancel()
+    await asyncio.gather(*tasks, return_exceptions=True)
+    await janus_q.async_q.put(_sentinel)
 
 
 asyncio.run(main())
